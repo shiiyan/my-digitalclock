1da939be91b7e7dbe4c025ea319b96c3c12cf42f
shiiyan <yan39123@gmail.com>
2021-08-13 22:27:12 +0900

    complete push model of observer pattern

--- src/main/kotlin/myDigitalClock/TimeSource.kt
+++ src/main/kotlin/myDigitalClock/TimeSource.kt
@@ -1,5 +1,15 @@
 package myDigitalClock
 
-interface TimeSource {
-    fun registerObserver(observer: ClockObserver)
+import java.util.Vector
+
+open class TimeSource {
+    private val itsObservers: Vector<ClockObserver> = Vector()
+
+    fun registerObserver(observer: ClockObserver) {
+        itsObservers.add(observer)
+    }
+
+    protected fun notify(hours: Int, minutes: Int, seconds: Int) {
+        itsObservers.forEach { it.update(hours, minutes, seconds) }
+    }
 }
--- src/test/kotlin/myDigitalClock/MockTimeSource.kt
+++ src/test/kotlin/myDigitalClock/MockTimeSource.kt
@@ -1,15 +1,7 @@
 package myDigitalClock
 
-import java.util.Vector
-
-class MockTimeSource : TimeSource {
-    private val itsObservers: Vector<ClockObserver> = Vector()
-
-    override fun registerObserver(observer: ClockObserver) {
-        itsObservers.add(observer)
-    }
-
+class MockTimeSource : TimeSource() {
     fun setTime(hours: Int, minutes: Int, seconds: Int) {
-        itsObservers.forEach { it.update(hours, minutes, seconds) }
+        notify(hours, minutes, seconds)
     }
 }
